92ea67dde7f5165d6d03cb755694bec39c2808c0
prateek-garg-mnr <prateek.garg@mckinleyrice.com>
2020-10-11 23:22:41 +0530

    Add error and success snackbars

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -11,6 +11,8 @@ import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 
 import requireAuth from "./requireAuth";
 import * as actions from "../actions";
@@ -18,6 +20,10 @@ import Loader from "./Loader";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
     "& .MuiTextField-root": {
       margin: theme.spacing(1),
       width: "30ch",
@@ -37,6 +43,10 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 function MessageForm(props) {
   // state
   // message text
@@ -48,6 +58,12 @@ function MessageForm(props) {
 
   // userType
   const [date, setDate] = useState("");
+
+  // error snackbar open close
+  const [errorOpen, setErrorOpen] = React.useState(false);
+
+  // success snackbar open close
+  const [successOpen, setSuccessOpen] = React.useState(false);
 
   // handle values
   // select channel name
@@ -76,9 +92,24 @@ function MessageForm(props) {
   const handleSubmit = () => {
     if (props.messageType === "instantMessage") {
       console.log("user type", userType);
-      props.sendInstantMessage(text, channel, userType, props.messageType);
+      props.loader(true);
+      props.sendInstantMessage(
+        text,
+        channel,
+        userType,
+        props.messageType,
+        props.history
+      );
     } else {
-      props.scheduleMessage(text, channel, userType, date, props.messageType);
+      props.loader(true);
+      props.scheduleMessage(
+        text,
+        channel,
+        userType,
+        date,
+        props.messageType,
+        props.history
+      );
     }
     console.log(date);
   };
@@ -86,8 +117,18 @@ function MessageForm(props) {
   // if no messageType then redirect to home
   useEffect(() => {
     if (!props.messageType) {
-      props.history.push("/");
+      props.history.push("/sendMessageoptions");
     }
+    if (props.messageStatus.response.response === true) {
+      setSuccessOpen(true);
+      setTimeout(() => {
+        props.history.push("/sendMessageoptions");
+      }, 2000);
+    }
+    if (props.messageStatus.error === true) {
+      setErrorOpen(true);
+    }
+
     console.log(props);
   });
 
@@ -111,123 +152,158 @@ function MessageForm(props) {
   return props.loading ? (
     <Loader />
   ) : (
-    <div className={`${classes.root} wrapper-main`}>
-      <div className="wrapper-sub">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.5, duration: 2 }}
-          className="options-heading"
-          style={{ marginTop: "-49px" }}
-        >
-          <p>Details</p>
-        </motion.div>
-        <div className="Main Options">
-          <ul className="option-List">
-            <motion.li
-              initial={{ x: "100vw" }}
-              animate={{ x: 0 }}
-              whileHover={{ scale: 1.1 }}
-              transition={{ type: "spring", stiffness: 300, delay: 0.3 }}
-              className="option-li-item"
-            >
-              {/* channel name */}
-              <FormControl className={classes.formControl}>
-                <InputLabel id="demo-simple-select-label">Channel</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={channel}
-                  onChange={handleChannelChange}
-                >
-                  {renderChannel()}
-                </Select>
-              </FormControl>
-            </motion.li>
-
-            <motion.li
-              initial={{ x: "100vw" }}
-              animate={{ x: 0 }}
-              whileHover={{ scale: 1.1 }}
-              transition={{ type: "spring", stiffness: 300, delay: 0.4 }}
-              className="option-li-item"
-            >
-              {/* Select type */}
-              <FormControl className={classes.formControl}>
-                <InputLabel id="demo-simple-select-label">Send As</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={userType}
-                  onChange={handleUserTypeChange}
-                >
-                  <MenuItem value={"bot"}>Bot</MenuItem>
-                  <MenuItem value={"user"}>User</MenuItem>
-                </Select>
-              </FormControl>
-            </motion.li>
-
-            <motion.li
-              initial={{ x: "100vw" }}
-              animate={{ x: 0 }}
-              whileHover={{ scale: 1.1 }}
-              transition={{ type: "spring", stiffness: 300, delay: 0.2 }}
-              className="option-li-item"
-            >
-              {/* message text */}
-              <TextField
-                id="standard-full-width"
-                label="Text"
-                style={{ margin: 8 }}
-                placeholder="Enter Message"
-                onChange={handleTextChange}
-                fullWidth
-                margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </motion.li>
-
-            {props.messageType === "instantMessage" ? (
-              ""
-            ) : (
+    <React.Fragment>
+      <Snackbar
+        open={errorOpen}
+        autoHideDuration={6000}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+      >
+        <Alert severity="error">
+          {props.messageType === "instantMessage"
+            ? "Unable to send message"
+            : "Unable to schedule message"}
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={successOpen}
+        autoHideDuration={6000}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+      >
+        <Alert severity="success">
+          {props.messageType === "instantMessage"
+            ? "Message Sent Successfully"
+            : "Message Scheduled Successfully"}
+        </Alert>
+      </Snackbar>
+      <div className={`${classes.root} wrapper-main`}>
+        <div className="wrapper-sub">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 2 }}
+            className="options-heading"
+            style={{ marginTop: "-49px" }}
+          >
+            <p>Details</p>
+          </motion.div>
+          <div className="Main Options">
+            <ul className="option-List">
               <motion.li
                 initial={{ x: "100vw" }}
                 animate={{ x: 0 }}
                 whileHover={{ scale: 1.1 }}
-                transition={{ type: "spring", stiffness: 300, delay: 0.5 }}
+                transition={{ type: "spring", stiffness: 300, delay: 0.3 }}
                 className="option-li-item"
               >
+                {/* channel name */}
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-simple-select-label">Channel</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={channel}
+                    onChange={handleChannelChange}
+                  >
+                    {renderChannel()}
+                  </Select>
+                </FormControl>
+              </motion.li>
+
+              <motion.li
+                initial={{ x: "100vw" }}
+                animate={{ x: 0 }}
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 300, delay: 0.4 }}
+                className="option-li-item"
+              >
+                {/* Select type */}
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-simple-select-label">Send As</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={userType}
+                    onChange={handleUserTypeChange}
+                  >
+                    <MenuItem value={"bot"}>Bot</MenuItem>
+                    <MenuItem value={"user"}>User</MenuItem>
+                  </Select>
+                </FormControl>
+              </motion.li>
+
+              <motion.li
+                initial={{ x: "100vw" }}
+                animate={{ x: 0 }}
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 300, delay: 0.2 }}
+                className="option-li-item"
+              >
+                {/* message text */}
                 <TextField
-                  id="datetime-local"
-                  label="Next appointment"
-                  type="datetime-local"
-                  onChange={handleDateChange}
-                  className={classes.textField}
+                  id="standard-full-width"
+                  label="Text"
+                  style={{ margin: 8 }}
+                  placeholder="Enter Message"
+                  onChange={handleTextChange}
+                  fullWidth
+                  margin="normal"
                   InputLabelProps={{
                     shrink: true,
                   }}
                 />
               </motion.li>
-            )}
-            <motion.li
-              initial={{ x: "100vw" }}
-              animate={{ x: 0 }}
-              whileHover={{ scale: 1.1 }}
-              transition={{ type: "spring", stiffness: 300, delay: 0.4 }}
-              className="option-li-item"
-              onClick={handleSubmit}
-            >
-              <button className="link btn">Submit</button>
-            </motion.li>
-          </ul>
+
+              {props.messageType === "instantMessage" ? (
+                ""
+              ) : (
+                <motion.li
+                  initial={{ x: "100vw" }}
+                  animate={{ x: 0 }}
+                  whileHover={{ scale: 1.1 }}
+                  transition={{ type: "spring", stiffness: 300, delay: 0.5 }}
+                  className="option-li-item"
+                >
+                  <TextField
+                    id="datetime-local"
+                    label="Next appointment"
+                    type="datetime-local"
+                    onChange={handleDateChange}
+                    className={classes.textField}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </motion.li>
+              )}
+              <motion.li
+                initial={{ x: "100vw" }}
+                animate={{ x: 0 }}
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 300, delay: 0.4 }}
+                className="option-li-item"
+                onClick={handleSubmit}
+              >
+                <button className="link btn">Submit</button>
+              </motion.li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
-const mapStateToProps = ({ messageType, loading, conversationList }) => {
-  return { messageType, loading, conversationList };
+const mapStateToProps = ({
+  messageType,
+  loading,
+  conversationList,
+  messageStatus,
+}) => {
+  return { messageType, loading, conversationList, messageStatus };
 };
 export default connect(mapStateToProps, actions)(requireAuth(MessageForm));
